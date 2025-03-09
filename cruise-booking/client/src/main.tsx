54722a3bc33d99faa8e7e3470ae0cf1a0615ec06
SOMSHUBHRA MAITY <121812035+somshubhra284@users.noreply.github.com>
2025-03-09 09:59:11 +0530

    Add cruise-booking/client/src/main.tsx 1741494557545

---
+++ cruise-booking/client/src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import axios from 'axios';
+
+// Configure axios defaults
+axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+// Add axios interceptor for authentication
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
