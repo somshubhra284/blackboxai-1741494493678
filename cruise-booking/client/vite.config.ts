70974e23e62c6e34cbdfae3f99e57353513a8071
SOMSHUBHRA MAITY <121812035+somshubhra284@users.noreply.github.com>
2025-03-09 09:59:01 +0530

    Add cruise-booking/client/vite.config.ts 1741494546986

---
+++ cruise-booking/client/vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost',
+        changeOrigin: true,
+      },
+    },
+    allowedHosts : ["t35cn3-5178.csb.app"]
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          utils: ['axios', 'zustand'],
+        },
+      },
+    },
+  },
+});
